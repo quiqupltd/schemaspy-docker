4c33b528a332b7caf2dd48404b6900f9fbe4b197
Ian Vaughan <ian@quiqup.com>
2018-07-25 02:02:39 +0100

    bind to 0.0.0.0

--- start.sh
+++ start.sh
@@ -69,7 +69,5 @@ if [ ! -f "$OUTPUT_PATH/index.html" ]; then
 	exit 1
 fi
 
-# busybox httpd
 echo "Starting webserver on port $SERVER_PORT"
-# httpd -f -p $SERVER_PORT -h "$OUTPUT_PATH"
-php5 -S localhost:$SERVER_PORT -t $OUTPUT_PATH
+php5 -S 0.0.0.0:$SERVER_PORT -t $OUTPUT_PATH
